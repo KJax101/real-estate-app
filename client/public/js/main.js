b9108f78a60401ca17b63512c28a06e05234ffd3
KJax101 <keng4105@gmail.com>
2018-08-14 19:24:22 -0400

    finished mobile styling. started js

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,2 +1,14 @@
-console.log("Hello Guys!");
-document.write("Hello Guys!");
+let hamIcon = document.querySelector("#ham-icon");
+function toggleMenu() {
+  // FIND IF MENU IS SHOWN OR HIDDEN
+  let expandedMenu = document.querySelector(".expanded-menu");
+  let currentStatus = expandedMenu.style.display;
+  // IF SHOWN, HIDE IT; IF HIDDEN, SHOW IT.
+  if (currentStatus === "block") {
+    expandedMenu.style.display = "none";
+  } else {
+    expandedMenu.style.display = "block";
+  }
+}
+
+hamIcon.addEventListener("click", toggleMenu);
