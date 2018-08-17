e0411203439dcba3659e9a7c160f9aa9f341b9f0
baibhavx <baibhavx@gmail.com>
2018-08-17 16:44:46 -0700

    Scrape data from forsalebyowner.com

--- real-estate-scraper/scraper.js
+++ real-estate-scraper/scraper.js
@@ -11,11 +11,58 @@ const scrapeDataFromFSBO = (city, state) => {
   const scrapeUrl = `${urlBase}${city}-${state}`;
   console.log(scrapeUrl);
   request(scrapeUrl, function(err, response, html) {
-    fs.writeFileSync("./filename.html", html);
+    fs.writeFileSync("./filename.html", html); 
+    const $ = cheerio.load(html); 
+
+    const myTitle = $("h1.hdg").text()   
+
+    const propertyList = $("ol.vList li").each(function(i, elem) { 
+      // ADDRESS 
+      let locationSpans = $(this).find('div.estateSummary-address span') 
+      let streetAddress = $(locationSpans[0]).text()   
+      let city = $(locationSpans[1]).text() 
+      let state = $(locationSpans[2]).text() 
+      let zipCode = $(locationSpans[3]).text()  
+
+      // PRICE 
+      let price = $(this).find("div.estateSummary-price").text() 
+      
+      // SUMMARY 
+      let summaries = $(this).find("div.estateSummary-list") 
+      let roomsCount = $(summaries[0]).text().replace(/\s/g, "");
+      let size = $(summaries[1]).text().replace(/\s/g, ""); 
+
+      //IMAGE 
+      let imageURL = $(this).find("div.imgBkgrd").attr("style") 
+
+      if(imageURL) {
+        // imageURL = imageURL.slice(23) 
+        imageURL = imageURL.slice(23, imageURL.length - 2)
+      } 
+
+      console.log(` 
+      Listing: ${i + 1}
+      Street: ${streetAddress}
+      City: ${city} 
+      State: ${state} 
+      Zip Code: ${zipCode} 
+      Price: ${price} 
+      Rooms: ${roomsCount} 
+      Size: ${size} 
+      ImageURL: ${imageURL}
+      `) 
+      console.log('\n')
+    })
+
+    console.log(myTitle)
+
+
+
+
   });
 };
 
 // www.forsalebyowner.com/search/list/beverly-hills-california
 // https://www.craigslist.org/tampa-fl/
 // TESTING BY USING FUNCTION
-scrapeDataFromFSBO("tampa", "florida");
+scrapeDataFromFSBO("berkeley", "california");
